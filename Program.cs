17d3042bade005a98a6fe88354ef4981851dd68e
Trang Le <tle20@usf.edu>
2021-02-05 01:21:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+            Hello_user(userName);
+        }
+
+        private static void Hello_user(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
+
+
